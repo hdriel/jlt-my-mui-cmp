f50e88a3296d1c312fec1846c116629c6a165d5e
hdriel <hdriel@gmail.com>
2023-04-04 23:08:13 +0300

    update avatar component

--- src/base-components/Avatar/Avatar.jsx
+++ src/base-components/Avatar/Avatar.jsx
@@ -1,8 +1,63 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Avatar as MuiAvatar } from "@mui/material";
+import { getCapitalLetters, stringToColor } from "../../utils/helpers";
 
-export default function Avatar() {
-    return (
-        <h1>Hello Avatar</h1>
-    );
+export default function Avatar({
+  username,
+  image,
+  size,
+  icon,
+  variant,
+  color,
+  fallbackImage,
+  onClick,
+}) {
+  const [url, setUrl] = useState(image);
+  const [fallbackSet, setFallbackSet] = useState(false);
+
+  const errorHandler = () => {
+    if (fallbackSet) {
+      setUrl(undefined);
+    } else {
+      setFallbackSet(true);
+      setUrl(fallbackImage);
+    }
+  };
+
+  const background =
+    color ?? (url && !username ? undefined : stringToColor(username));
+
+  return (
+    <MuiAvatar
+      alt={username ?? "avatar"}
+      src={url}
+      sx={{ width: size, height: size, background }}
+      variant={variant}
+      imgProps={{ onError: errorHandler }}
+      onClick={onClick}
+    >
+      {!url && (icon ?? getCapitalLetters(username))}
+    </MuiAvatar>
+  );
 }
+
+Avatar.propTypes = {
+  username: PropTypes.string,
+  image: PropTypes.string,
+  size: PropTypes.string,
+  icon: PropTypes.string,
+  variant: PropTypes.oneOf(["circular", "rounded", "square"]),
+  fallbackImage: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+Avatar.defaultProps = {
+  username: undefined,
+  image: undefined,
+  size: undefined,
+  icon: undefined,
+  variant: "circular",
+  fallbackImage: undefined,
+  onClick: undefined,
+};
--- src/base-components/Avatar/__stories__/Avatar.stories.jsx
+++ src/base-components/Avatar/__stories__/Avatar.stories.jsx
@@ -1,27 +1,58 @@
-import React from 'react';
-import Avatar from '../Avatar';
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import { Send as SendIcon } from "@mui/icons-material";
+
+import Avatar from "../Avatar";
 
 export default {
-    title: 'base-components/Avatar',
-    component: Avatar
-}
+  title: "base-components/Avatar",
+  component: Avatar,
+};
+
+const actions = {
+  onClick: action("onClick"),
+};
 
 export const Default = () => {
-    return <Avatar />
-}
+  return <Avatar {...actions} />;
+};
+
+export const Colored = () => {
+  return <Avatar {...actions} color={"#174c05"} />;
+};
 
 export const Name = () => {
-    return <Avatar />
-}
+  return <Avatar {...actions} username="Hadriel  Benjo" />;
+};
+
+export const LongName = () => {
+  return <Avatar {...actions} username="Hadriel Benjo And Afek " />;
+};
 
 export const Image = () => {
-    return <Avatar />
-}
+  return <Avatar {...actions} image="/1.jpg" />;
+};
 
-export const Online = () => {
-    return <Avatar />
-}
+export const BrokenImage = () => {
+  return (
+    <Avatar {...actions} image="/broken-image.jpg" fallbackImage="/cat.jpg" />
+  );
+};
 
-export const Offline = () => {
-    return <Avatar />
-}
+export const BrokenBrokenImage = () => {
+  return (
+    <Avatar
+      {...actions}
+      image="/broken-image.jpg"
+      fallbackImage="/undefined.jpg"
+    />
+  );
+};
+
+export const Icon = () => {
+  return <Avatar {...actions} icon={<SendIcon />} />;
+};
+
+export const Square = () => {
+  return <Avatar {...actions} variant="square" icon={<SendIcon />} />;
+};
